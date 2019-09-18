9e18f5d7ad6e4f9b670ff91df99a802caed23993
andres9906 <stivenaereo@gmail.com>
2019-09-18 13:41:40 -0500

    components

---
+++ src/components/log_in/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function log_in() {
+    return(
+        <div className="log_in">
+        <div className="container">
+            <h1>Inicia sesión en Twitter</h1>
+            <input type="button" placeholder="telefono, correo o usuario"> </input>
+        </div>
+        </div>
+    )
+}
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload......
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
